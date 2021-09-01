7a000415acad2cb43233446f2d695ac10b03d07b
Blur-tec <blurtec@protonmail.com>
2021-09-01 22:43:29 +0200

    renamed Index.php => index.php

---
+++ index.php
@@ -0,0 +1,200 @@
+<?php
+include ("DatabaseConnect.php");
+include ("CDBTables.php");
+include ("DatabaseConnect.php");
+include ("CDBTables.php");
+include ("ConnectionStringIncDB.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>B-Empowered</title>
+    <link rel="shortcut icon" href="assets/img/fav-icon.ico" />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="text-end">
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="padding-top: 0;padding-bottom: 0;margin-top: 10px;margin-bottom: 20px;">
+            <div class="container">
+              <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
+                <span class="visually-hidden">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <a href="index.php"><img src="assets/images/logo-green.png" style="width: 66px;height: 53.4px;margin: 1px;padding: 1px;"></a>
+                <div class="collapse navbar-collapse text-start" id="navcol-1">
+                    <ul class="navbar-nav me-auto">
+                        
+                    </ul><span class="navbar-text actions"> 
+					<a class="text-end login" href="ULogin.php" style="color: #22a900;">Log In</a>
+					<a class="btn btn-light text-end action-button" role="button" href="RegistrationPage.php" style="background: #22a900;">Register</a></span>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div><img src="assets/img/HebronMall.jpg" class="home_image" /></div>
+    <div class="text-center">
+      <a href="RegistrationPage.php">
+    <button class="opportunities_button" type="button">
+        Opportunities at HEBRON MALL
+      </button>
+</a>
+    </div>
+    <div>
+      <p class="home_paragraph">
+      B‐empowered is an online turnkey empowerment medium aims to bring opportunities, information, 
+knowledge, people and resources at the finger tips off opportunity seekers, corporates, local human 
+capital and related pool of needy users. 
+ 
+This online turnkey medium serves as one of our local corporate endeavour towards capping the 
+spread  of  COVID‐19  in  township  based  mega‐projects;  and  this  is  achieved  through  strategic 
+deployment of e‐solutions tailor‐made to address socio‐economic empowerment needs of local 
+content.  
+      </p>
+    </div>
+    <div class="text-center">
+      <a href="about.html" style="font-size: 11px">Read more</a>
+    </div>
+    <div class="text-center" style="margin-top: 20px">
+      <div class="container" style="margin-bottom: 20px">
+        <div class="row">
+          <div class="col-md-3">
+            <a href="opportunities.html">
+              <h1
+                class="text-center"
+                style="
+                  background: #22a900;
+                  color: rgb(255, 255, 255);
+                  font-size: 17px;
+                  border-radius: 5px;
+                "
+              >
+                Job Opportunities
+              </h1>
+              <img src="assets/img/264241605.jpg" style="width: 200px" />
+            </a>
+          </div>
+          <div class="col-md-3">
+            <h1
+              class="text-center"
+              style="
+                background: #22a900;
+                color: rgb(255, 255, 255);
+                font-size: 17px;
+                  border-radius: 5px;
+              "
+            >
+              Skills Opportunities
+            </h1>
+            <img
+              src="assets/img/shutterstock_1270338166_small.jpg"
+              style="width: 200px"
+            />
+          </div>
+          <div class="col-md-3">
+            <h1
+              class="text-center"
+              style="
+                background: #22a900;
+                color: rgb(255, 255, 255);
+                font-size: 17px;
+                  border-radius: 5px;
+              "
+            >
+              Enterprise Development
+            </h1>
+            <img
+              src="assets/img/ea25256e078f43a4850c097d399d609b.jpg"
+              style="width: 200px"
+            />
+          </div>
+          <div class="col-md-3">
+            <h1
+              class="text-center"
+              style="
+                background: #22a900;
+                color: rgb(255, 255, 255);
+                font-size: 17px;
+                  border-radius: 5px;
+              "
+            >
+              Corporate Social Development
+            </h1>
+            <img
+              src="assets/img/istockphoto-983236530-612x612.jpg"
+              style="width: 200px"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center" style="margin-top: 20px">
+      <h1
+        style="color: #ffffff; background: #22a900; margin-top: 20px; margin-bottom: 20px; padding: 15px;"
+      >
+        Key Partners
+      </h1>
+    </div>
+    <div class="text-center" style="margin-top: 20px">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <img
+              src="assets/img/B-empowered-logo-Icon-green.png"
+              style="width: 140px"
+              alt="brand-name"
+            />
+          </div>
+          <div class="col-md-3">
+            <img
+              src="assets/img/B-empowered-logo-Icon-green.png"
+              style="width: 140px"
+              alt="brand-name"
+            />
+          </div>
+          <div class="col-md-3">
+            <img
+              src="assets/img/B-empowered-logo-Icon-green.png"
+              style="width: 140px"
+              alt="brand-name"
+            />
+          </div>
+          <div class="col-md-3">
+            <img
+              src="assets/img/B-empowered-logo-Icon-green.png"
+              style="width: 140px"
+              alt="brand-name"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <footer
+        class="footer-basic"
+        style="background: #22a900; padding-top: 0; padding-bottom: 0"
+      >
+        <p class="copyright" style="color: rgb(255, 255, 255); margin-top: 0">
+          © 2020 - 2021 | GenerisLab. All Rights Reserved.
+        </p>
+      </footer>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+</body>
+
+</html>
